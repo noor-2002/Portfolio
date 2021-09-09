fa388b6bfa99b5425927863ddd9b8658efd32f8c
NoorMohamed-intern <NoorMohamed-intern>
2021-09-09 11:32:16 +0530

    initial commit

---
+++ src/pages/projects/Projects.js
@@ -0,0 +1,257 @@
+import React, { Component } from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
+import PublicationCard from "../../components/publicationsCard/PublicationCard";
+import Button from "../../components/button/Button";
+import TopButton from "../../components/topButton/TopButton";
+import { Fade } from "react-reveal";
+import { projectsHeader, publicationsHeader } from "../../portfolio.js";
+import ProjectsData from "../../shared/opensource/projects.json";
+import PublicationData from "../../shared/opensource/publications.json";
+import "./Projects.css";
+import ProjectsImg from "./ProjectsImg";
+
+class Projects extends Component {
+  render() {
+    const theme = this.props.theme;
+    return (
+      <div className="projects-main">
+        <Header theme={theme} />
+        <div className="basic-projects">
+          <Fade bottom duration={2000} distance="40px">
+            <div className="projects-heading-div">
+              <div className="projects-heading-img-div">
+                {/* <img
+											src={require(`../../assests/images/${projectsHeader["avatar_image_path"]}`)}
+											alt=""
+										/> */}
+                <ProjectsImg theme={theme} />
+              </div>
+              <div className="projects-heading-text-div">
+                <h1
+                  className="projects-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {projectsHeader.title}
+                </h1>
+                <p
+                  className="projects-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {projectsHeader["description"]}
+                </p>
+              </div>
+            </div>
+          </Fade>
+        </div>
+        <div className="repo-cards-div-main">
+          <div>
+            <Fade bottom duration={2000} distance="40px">
+              <div
+                className="repo-card-div"
+                // key={repo.id}
+                // onClick={() => openRepoinNewTab(repo.url)}
+                onClick={() =>
+                  window.open("https://bishops.co.in/bishops-service/")
+                }
+                style={{ backgroundColor: theme.highlight }}
+              >
+                <div className="repo-name-div">
+                  <p
+                    className="repo-name"
+                    style={{ color: theme.imageHighlight }}
+                  >
+                    Bishops
+                  </p>
+                </div>
+                <p className="repo-description" style={{ color: theme.text }}>
+                  The Bishops Service Program is designed to offer complete
+                  after-sales support to the customer consisting of
+                  Installation.
+                </p>
+                <div className="repo-details">
+                  <p
+                    className="repo-creation-date subTitle"
+                    style={{ color: theme.secondaryText }}
+                  >
+                    Created on 25-07-2021
+                  </p>
+                  {/* <ProjectLanguages
+                    className="repo-languages"
+                    // logos={repo.languages}
+                  /> */}
+                </div>
+              </div>
+            </Fade>
+          </div>
+          <div>
+            <Fade bottom duration={2000} distance="40px">
+              <div
+                className="repo-card-div"
+                // key={repo.id}
+                // onClick={() => openRepoinNewTab(repo.url)}
+                onClick={() => window.open("http://www.psriconvalley.com")}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://your_url_here.html"
+                style={{ backgroundColor: theme.highlight }}
+              >
+                <div className="repo-name-div">
+                  <p
+                    className="repo-name"
+                    style={{ color: theme.imageHighlight }}
+                  >
+                    PSR Icon valley
+                    {/* {repo.name} */}
+                  </p>
+                </div>
+                <p className="repo-description" style={{ color: theme.text }}>
+                  Located off Jigani-Anekal Road, PSR Icon Valley is a 38 Acre,
+                  lush green, gated community of residential plots.
+                  {/* {repo.description} */}
+                </p>
+                <div className="repo-details">
+                  <p
+                    className="repo-creation-date subTitle"
+                    style={{ color: theme.secondaryText }}
+                  >
+                    {/* Created on {repo.createdAt.split("T")[0]} */}
+                    Created on 14-06-2021
+                  </p>
+                  {/* <ProjectLanguages
+                    className="repo-languages"
+                    // logos={repo.languages}
+                  /> */}
+                </div>
+              </div>
+            </Fade>
+          </div>
+          <div>
+            <Fade bottom duration={2000} distance="40px">
+              <div
+                className="repo-card-div"
+                // key={repo.id}
+                // onClick={() => openRepoinNewTab(repo.url)}
+                onClick={() => window.open("https://barbequeelite.com")}
+                style={{ backgroundColor: theme.highlight }}
+              >
+                <div className="repo-name-div">
+                  <p
+                    className="repo-name"
+                    style={{ color: theme.imageHighlight }}
+                  >
+                    Barbequeelite
+                    {/* {repo.name} */}
+                  </p>
+                </div>
+                <p className="repo-description" style={{ color: theme.text }}>
+                  Our primary goal as a barbecue restaurant is your
+                  satisfaction! With every dish, meal, and barbecue we make, we
+                  do our best to ensure that it is.
+                  {/* {repo.description} */}
+                </p>
+                <div className="repo-details">
+                  <p
+                    className="repo-creation-date subTitle"
+                    style={{ color: theme.secondaryText }}
+                  >
+                    {/* Created on {repo.createdAt.split("T")[0]} */}
+                    Created on 09-08-2021
+                  </p>
+                  {/* <ProjectLanguages
+                    className="repo-languages"
+                    // logos={repo.languages}
+                  /> */}
+                </div>
+              </div>
+            </Fade>
+          </div>
+          <div>
+            <Fade bottom duration={2000} distance="40px">
+              <div
+                className="repo-card-div"
+                // key={repo.id}
+                // onClick={() => openRepoinNewTab(repo.url)}
+                onClick={() => window.open("https://forkify-noor.netlify.app")}
+                style={{ backgroundColor: theme.highlight }}
+              >
+                <div className="repo-name-div">
+                  <p
+                    className="repo-name"
+                    style={{ color: theme.imageHighlight }}
+                  >
+                    Forkify
+                    {/* {repo.name} */}
+                  </p>
+                </div>
+                <p className="repo-description" style={{ color: theme.text }}>
+                  This project is purely done by Vanilla JS. We fetch all the
+                  dishes from a API and even the user and add their own Dishes.
+                  {/* {repo.description} */}
+                </p>
+                <div className="repo-details">
+                  <p
+                    className="repo-creation-date subTitle"
+                    style={{ color: theme.secondaryText }}
+                  >
+                    {/* Created on {repo.createdAt.split("T")[0]} */}
+                    Created on 09-08-2021
+                  </p>
+                  {/* <ProjectLanguages
+                    className="repo-languages"
+                    // logos={repo.languages}
+                  /> */}
+                </div>
+              </div>
+            </Fade>
+          </div>
+          {/* {ProjectsData.data.map((repo) => {
+            return <GithubRepoCard repo={repo} theme={theme} />;
+          })} */}
+        </div>
+        <Button
+          text={"More Projects"}
+          className="project-button"
+          href="https://github.com/NoorMohamed-intern"
+          newTab={true}
+          theme={theme}
+        />
+
+        {/* Publications  */}
+
+        {/* <div className="basic-projects">
+          <Fade bottom duration={2000} distance="40px">
+            <div className="publications-heading-div">
+              <div className="publications-heading-text-div">
+                <h1
+                  className="publications-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {publicationsHeader.title}
+                </h1>
+                <p
+                  className="projects-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {publicationsHeader["description"]}
+                </p>
+              </div>
+            </div>
+          </Fade>
+        </div>
+
+        <div className="repo-cards-div-main">
+          {PublicationData.data.map((repo) => {
+            return <PublicationCard repo={repo} theme={theme} />;
+          })}
+        </div> */}
+
+        <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
+        <TopButton theme={this.props.theme} />
+      </div>
+    );
+  }
+}
+
+export default Projects;
---
+++ src/portfolio.js
@@ -0,0 +1,347 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Noor's Portfolio",
+  description:
+    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  og: {
+    title: "Noor mohamed Portfolio",
+    type: "website",
+    url: "http://noor.com/",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Noor Mohamed",
+  logo_name: "Noor mohamed",
+  nickname: "",
+  subTitle:
+    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  resumeLink:
+    "https://drive.google.com/file/d/1xomyFMTG1UHrQVfgCHcUw6sUOEU2SW7e/view?usp=sharing",
+  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
+};
+
+const socialMediaLinks = [
+  {
+    name: "Github",
+    link: "https://github.com/NoorMohamed-intern",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/noor-mohamed-746a30212/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+
+  {
+    name: "Gmail",
+    link: "mailto:noormd.intern@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/noorm1995",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/_i_dont_use_emojis_/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Web Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building resposive website front end using React-Redux",
+        "⚡ Developing mobile applications using Flutter, React Native and solo android apps using Kotlin",
+        "⚡ Creating application backend in Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Sass",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Yarn",
+          fontAwesomeClassname: "simple-icons:yarn",
+          style: {
+            color: "#2C8EBB",
+          },
+        },
+      ],
+    },
+
+    {
+      title: "UI/UX Design",
+      fileName: "DesignImg",
+      skills: [
+        "⚡ Designing highly attractive user interface for mobile and web applications",
+        "⚡ Customizing logo designs and building logos from scratch",
+        "⚡ Creating the flow of application functionalities to optimize user experience",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Adobe XD",
+          fontAwesomeClassname: "simple-icons:adobexd",
+          style: {
+            color: "#FF2BC2",
+          },
+        },
+        {
+          skillName: "Figma",
+          fontAwesomeClassname: "simple-icons:figma",
+          style: {
+            color: "#F24E1E",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    // {
+    //   siteName: "HackerRank",
+    //   iconifyClassname: "simple-icons:hackerrank",
+    //   style: {
+    //     color: "#2EC866",
+    //   },
+    //   profileLink: "",
+    // },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Aalim muhammad salegh college of engineering",
+      subtitle: "B.E. in Computer Engineering",
+      logo_path: "amslogo-1.png",
+      alt_name: "IIITDM Kurnool",
+      duration: "2019 - 2022",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on HTML, Sass, Git, JS, React",
+        "⚡ I was selected for NSP Means Scholarship which is given to top students of our college. I have received award from respected director for consistently best performance in academics.",
+      ],
+      website_link: "https://www.aalimec.ac.in",
+    },
+    {
+      title: "Madras christian college higher secondary school",
+      subtitle: "High school",
+      logo_path: "MCC-LOGO-3.jpg",
+      alt_name: "IIITDM Kurnool",
+      duration: "2017 - 2019",
+      descriptions: [
+        "⚡ I have passed my 12th grade in MCC, chennai (English medium).",
+        "⚡ I have also learned basics of programming here and many more",
+        "⚡ I have participated in multiple programs and educational trips during my school days",
+      ],
+      website_link: "https://mcc.edu.in",
+    },
+    {
+      title: "St.anthonys high secondary school",
+      subtitle: "High school",
+      logo_path: "st_anthonys_logo-removebg-preview-3.png",
+      alt_name: "IIITDM Kurnool",
+      duration: "2017 - 2019",
+      descriptions: [
+        "⚡ I have passed my 10th grade in st anthonys, chennai (English medium).",
+        "⚡ I have also learned basics of programming here and many more",
+        "⚡ I have participated in multiple programs and educational trips during my school days",
+      ],
+      website_link: "https://stanthonysegmore.org",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Internshala",
+      subtitle: "- web development",
+      logo_path: "internshala.jpg",
+      certificate_link: "https://internshala.com/verify_certificate/",
+      alt_name: "Internshala certificate",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "Udemy",
+      subtitle: "- Andrew Ng",
+      logo_path: "deeplearning_ai_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
+      alt_name: "JavaScript",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "ML on GCP",
+      subtitle: "- GCP Training",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/EB4VJARK8647",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Web development Intern",
+          company: "Moxman Pvt. Ltd.",
+          company_url: "https://moxman.com",
+          logo_path: "Moxman-Design-1.png",
+          duration: "Jun 2019 - Aug 2019",
+          location: "Chennai, Tamilnadu",
+          description:
+            "I had been working as developer at moxman. I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
+          color: "#ee3c26",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Google Explore ML Facilitator",
+          company: "Google",
+          company_url: "https://about.google/",
+          logo_path: "google_logo.png",
+          duration: "June 2019 - April 2020",
+          location: "Hyderabad, Telangana",
+          description:
+            "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
+          color: "#4285F4",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
+  avatar_image_path: "projects_image.svg",
+};
+
+const publicationsHeader = {
+  title: "Publications",
+  description:
+    "I have worked on and published a few research papers and publications of my own.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "noor-photo-6.jpg",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+  },
+  blogSection: {
+    // title: "Blogs",
+    // subtitle:
+    //   "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+    // link: "",
+    // avatar_image_path: "blogs_image.svg",
+  },
+  addressSection: {
+    title: "Address",
+    subtitle: "Admbakkam, chennai, Tamilnadu, Chennai - 600088",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "https://goo.gl/maps/Xv3vnGPrkc91Vify6",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "+91 9361932044",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  publicationsHeader,
+  contactPageData,
+};
