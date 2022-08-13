44c0ea536a7e1359027d9b66afca238c87388460
NoorMohamed-intern <noormd.intern@gmail.com>
2022-08-14 00:08:13 +0530

    couser change

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
+import CustomCursor from "custom-cursor-react";
+import "custom-cursor-react/dist/index.css";
 
 function App() {
   return (
-    <ThemeProvider theme={chosenTheme}>
-      <>
-        <GlobalStyles />
-        <div>
-          <Main theme={chosenTheme} />
-        </div>
-      </>
-    </ThemeProvider>
+    <div>
+      <CustomCursor
+        targets={[".link", ".custom-css"]}
+        customClass="custom-cursor"
+        dimensions={50}
+        fill="rgb(252, 163, 17)"
+        smoothness={{
+          movement: 0.2,
+          scale: 0.1,
+          opacity: 1,
+        }}
+        targetOpacity={0.5}
+      />
+
+      <ThemeProvider theme={chosenTheme}>
+        <>
+          <GlobalStyles />
+          <div>
+            <Main theme={chosenTheme} />
+          </div>
+        </>
+      </ThemeProvider>
+    </div>
   );
 }
 
